934ce42e5ec0fc44a3ab329c0023b6831a1aa1f5
Vaibhav Todi <vaibhav@deeproot.in>
2016-04-18 12:03:26 +0530

    Updating the entrypoint startup script for running the slapd service

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ exec 2>&1
 set -e
 
 #  Starting the java process
-exec  /usr/sbin/slapd -d  0 -h "ldap://0.0.0.0:389/ ldaps://0.0.0.0:636/"
+exec  /usr/sbin/slapd -h ldap:/// ldaps:/// -g openldap -u openldap -f /etc/ldap/slapd.conf
